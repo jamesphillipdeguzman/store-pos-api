e6f3fee6508ab1823dcf09186299217bf104a0aa
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-05-31 21:08:11 +0800

    feat: add all models (e.g., customer, product, sale, and user)

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    googleId: {
+      // assign from profile.id
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: /.+@.+\..+/, // Basic email pattern
+      trim: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['admin', 'cashier', 'manager'],
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }, // for automatic createdAt and updatedAt fields
+);
+
+export default mongoose.model('User', userSchema);
